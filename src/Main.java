091f8049029228aa3b47d50b982565d44def016f
Nechaev.D <denisnechaev567@gmail.com>
2025-05-26 15:52:58 +0300

    hw5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         int os = 3;
         if (os == 0) {
             System.out.println("Установите версию приложения для OS");
